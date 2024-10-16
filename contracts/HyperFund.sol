0c38f2332313b223c28c173b21a6d34b7fca6831
Liron Achdut <liron.achdut@gmail.com>
2024-10-16 16:01:07 +0700

    onERC20Received -> donate

--- contracts/HyperFund.sol
+++ contracts/HyperFund.sol
@@ -1,52 +1,52 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.17;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
-// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import {HypercertMinter} from "hypercert/contracts/src/protocol/HypercertMinter.sol";
-// import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {HypercertMinter} from "hypercert/contracts/src/protocol/HypercertMinter.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-// contract HyperFund is HypercertMinter, Ownable, Safe {
-//     IERC20 public erc20Token;
-//     uint256 public tokenID;
-//     uint256 public typeID;
-//     uint256 public units;
-//     address[] public beneficiaries;
-//     uint256[] public fractions;
+contract HyperFund is HypercertMinter, Ownable, Safe {
+    IERC20 public erc20Token;
+    uint256 public tokenID;
+    uint256 public typeID;
+    uint256 public units;
+    address[] public beneficiaries;
+    uint256[] public fractions;
 
-//     event DonationReceived(address donor, uint256 amount);
+    event DonationReceived(address donor, uint256 amount);
 
-//     constructor(address _erc20Token) {
-//         erc20Token = IERC20(_erc20Token);
-//     }
+    constructor(address _erc20Token) {
+        erc20Token = IERC20(_erc20Token);
+    }
 
-//     // ADMIN FUNCTIONS
+    // ADMIN FUNCTIONS
 
-//     function startCampaign(uint256 units, string memory uri) external onlyOwner {
-//         // TODO check that last campaign is finished?
-//         mintClaim(owner(), units, uri, TransferRestrictions.AllowAll);
-//         typeID = typeCounter << 128;
-//         tokenID = typeID + maxIndex[typeID];
-//     }
+    function startCampaign(uint256 units, string memory uri) external onlyOwner {
+        // TODO check that last campaign is finished?
+        mintClaim(owner(), units, uri, TransferRestrictions.AllowAll);
+        typeID = typeCounter << 128;
+        tokenID = typeID + maxIndex[typeID];
+    }
 
-//     function setBeneficiaries(address[] memory _beneficiaries, uint256[] memory _fractions) external onlyOwner {
-//         require(beneficiaries.length == fractions.length, "Beneficiaries and fractions length mismatch");
-//         beneficiaries = _beneficiaries;
-//         fractions = _fractions;
-//     }
+    function setBeneficiaries(address[] memory _beneficiaries, uint256[] memory _fractions) external onlyOwner {
+        require(beneficiaries.length == fractions.length, "Beneficiaries and fractions length mismatch");
+        beneficiaries = _beneficiaries;
+        fractions = _fractions;
+    }
 
-//     // PUBLIC FUNCTIONS
+    // PUBLIC FUNCTIONS
 
-//     function onERC20Received(address from, uint256 amount) external {
-//         require(msg.sender == address(erc20Token), "Only specific ERC20 token allowed");
-//         _mintFraction(from, amount);
-//         emit DonationReceived(from, amount);
-//     }
+    function donate(address from, uint256 amount) external {
+        erc20Token.transferFrom(msg.sender, address(this), amount);
+        _mintFraction(from, amount);
+        emit DonationReceived(from, amount);
+    }
 
-//     // INTERNAL FUNCTIONS
+    // INTERNAL FUNCTIONS
 
-//     function _mintFraction(address account, uint256 amount) internal {
-//         splitFraction(owner(), tokenID, [amount, tokenValues[tokenID] - amount]); // this results in tokenID now holding the new amount
-//         _safeTransferFrom(owner(), account, tokenID, amount, "");
-//         tokenID = typeID + maxIndex[typeID]; // next token now holds the rest of the amount
-//     }
-// }
+    function _mintFraction(address account, uint256 amount) internal {
+        splitFraction(owner(), tokenID, [amount, tokenValues[tokenID] - amount]); // this results in tokenID now holding the new amount
+        _safeTransferFrom(owner(), account, tokenID, amount, "");
+        tokenID = typeID + maxIndex[typeID]; // next token now holds the rest of the amount
+    }
+}
