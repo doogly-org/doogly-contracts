92d328a833da4a566aa5f7ac529fff71f9173afb
Prajjawalk <prajjawalkhandelwal@gmail.com>
2024-11-26 22:34:27 +0530

    Feature: output token customisations

--- contracts/CallContractWithToken.sol
+++ contracts/CallContractWithToken.sol
@@ -10,6 +10,7 @@ import { IUniswapV3Factory } from '@uniswap/v3-core/contracts/interfaces/IUniswa
 import { IUniswapV3Pool } from '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IHypercertToken {
     function splitFraction(
@@ -33,7 +34,7 @@ interface ISwapRouter is IV3SwapRouter {
  * @title Call Contract With Token 
  * @notice Send a token along with an Axelar GMP message between two blockchains
  */
-contract SwapperBridger is AxelarExecutable, ReentrancyGuard {
+contract SwapperBridger is AxelarExecutable, ReentrancyGuard, Ownable {
     using Strings for bytes;
     using Strings for uint256;
     IAxelarGasService public immutable gasService;
@@ -47,6 +48,19 @@ contract SwapperBridger is AxelarExecutable, ReentrancyGuard {
         uint256 hypercertFractionId,
         address fractionRecipient
     );
+
+    event HypercertTransferFailed(
+      bytes reason
+    );
+
+    event SwapFailed(
+      address inputToken,
+      address outputToken,
+      bytes reason
+    );
+
+    event AlloPoolFunded (uint256 poolId, uint256 amount, address token);
+    event AlloPoolFundingFailed (uint256 poolId, uint256 amount, address token, bytes reason);
 
     // Define the Uniswap V3 Factory address
     address public UNISWAP_V3_FACTORY;
@@ -65,7 +79,7 @@ contract SwapperBridger is AxelarExecutable, ReentrancyGuard {
      * @param _gateway address of axl gateway on deployed chain
      * @param _gasReceiver address of axl gas service on deployed chain
      */
-    constructor(address _gateway, address _gasReceiver, address _uniswapV3Factory, address _uniswapV3Router, address _usdc, address _axlUSDC, address _hypercertContract) AxelarExecutable(_gateway) {
+    constructor(address _gateway, address _gasReceiver, address _uniswapV3Factory, address _uniswapV3Router, address _usdc, address _axlUSDC, address _hypercertContract) AxelarExecutable(_gateway) Ownable() {
         gasService = IAxelarGasService(_gasReceiver);
         UNISWAP_V3_FACTORY = _uniswapV3Factory;
         UNISWAP_V3_ROUTER = _uniswapV3Router;
@@ -175,6 +189,76 @@ contract SwapperBridger is AxelarExecutable, ReentrancyGuard {
         return axlUsdcAmount;
     }
 
+    /**
+     * @notice swap token to USDC using uniswap v3 router
+     * @param _outputTokenAddress address of token being sent
+     * @param _amount amount of tokens being sent
+     * @param _receiverAddress address of the receiver
+     */
+    function _swapTokenDestination(address _outputTokenAddress, uint256 _amount, address _receiverAddress) internal returns (uint256, address) {
+
+        require(IERC20(AXL_USDC).balanceOf(address(this)) >= _amount, "Insufficient axlUSDC balance");
+
+        if (_outputTokenAddress == AXL_USDC) {
+            // Transfer axlUSDC tokens to user
+            require(IERC20(AXL_USDC).transfer(_receiverAddress, _amount), "Transfer failed");
+
+            return (_amount, AXL_USDC);
+        }
+
+        // Approve the router to spend axlUSDC
+        require(IERC20(AXL_USDC).approve(UNISWAP_V3_ROUTER, _amount), "Approval failed");
+
+        uint256 tokenAmount;
+
+        bytes memory path;
+
+        if (_outputTokenAddress == USDC) {
+            // If output is USDC, do single swap to USDC
+            path = abi.encodePacked(
+                AXL_USDC,
+                findBestFeeTierNoRevert(AXL_USDC, USDC),
+                USDC
+            );
+        } else {
+            // For other tokens, do multihop swap: token -> USDC -> axlUSDC
+            path = abi.encodePacked(
+                AXL_USDC,
+                findBestFeeTierNoRevert(AXL_USDC, USDC),
+                USDC, 
+                findBestFeeTierNoRevert(USDC, _outputTokenAddress), 
+                _outputTokenAddress
+            );
+        }
+
+        // Set up the parameters for the swap
+        ISwapRouter.ExactInputParams memory params = IV3SwapRouter.ExactInputParams({
+            path: path,
+            recipient: _receiverAddress,
+            // deadline: block.timestamp + 15 minutes,
+            amountIn: _amount,
+            amountOutMinimum: 0 // As requested, keeping this at 0
+        });
+
+        // Execute the swap
+        try ISwapRouter(UNISWAP_V3_ROUTER).exactInput(params) returns (uint256 amountOut) {
+            tokenAmount = amountOut;
+            // Emit an event with swap details
+            emit TokenSwapped(AXL_USDC, _outputTokenAddress, _amount, tokenAmount);
+            // Return the final amount of output token
+            return (tokenAmount, _outputTokenAddress);
+        } catch (bytes memory reason) {
+            // Fallback to transferring axlUSDC to receiver
+            require(IERC20(AXL_USDC).transfer(_receiverAddress, _amount), "Transfer failed");
+            tokenAmount = _amount;
+            emit SwapFailed(AXL_USDC, _outputTokenAddress, reason);
+            // Return the final amount of output token
+            return (tokenAmount, AXL_USDC);
+        }
+
+        
+    }
+
     function findBestFeeTier(address tokenA, address tokenB) internal view returns (uint24) {
         uint24[4] memory feeTiers = [uint24(500), uint24(3000), uint24(10000), uint24(100)]; // 0.05%, 0.3%, 1%, 0.01%
         uint128 highestLiquidity = 0;
@@ -192,6 +276,25 @@ contract SwapperBridger is AxelarExecutable, ReentrancyGuard {
         }
         
         require(bestFeeTier != 0, "No valid pool found for the swap");
+        return bestFeeTier;
+    }
+
+    function findBestFeeTierNoRevert(address tokenA, address tokenB) internal view returns (uint24) {
+        uint24[4] memory feeTiers = [uint24(500), uint24(3000), uint24(10000), uint24(100)]; // 0.05%, 0.3%, 1%, 0.01%
+        uint128 highestLiquidity = 0;
+        uint24 bestFeeTier = 0;
+        
+        for (uint i = 0; i < feeTiers.length; i++) {
+            address poolAddress = IUniswapV3Factory(UNISWAP_V3_FACTORY).getPool(tokenA, tokenB, feeTiers[i]);
+            if (poolAddress != address(0)) {
+                uint128 liquidity = IUniswapV3Pool(poolAddress).liquidity();
+                if (liquidity > highestLiquidity) {
+                    highestLiquidity = liquidity;
+                    bestFeeTier = feeTiers[i];
+                }
+            }
+        }
+        
         return bestFeeTier;
     }
 
@@ -224,8 +327,9 @@ contract SwapperBridger is AxelarExecutable, ReentrancyGuard {
         address _splitsAddress,
         uint256 _hypercertFractionId,
         address inputTokenAddress,
+        address destinationOutputTokenAddress,
         uint256 amount
-    ) external payable {
+    ) nonReentrant external payable {
         require(msg.value > 0, 'Gas payment is required');
 
         uint256 swappedAmount = _swapToken(inputTokenAddress, amount);
@@ -233,7 +337,7 @@ contract SwapperBridger is AxelarExecutable, ReentrancyGuard {
         address tokenAddress = gateway.tokenAddresses('axlUSDC');
 
         IERC20(tokenAddress).approve(address(gateway), swappedAmount);
-        bytes memory payload = abi.encode(hcRecipientAddress, _splitsAddress, _hypercertFractionId, poolId);
+        bytes memory payload = abi.encode(hcRecipientAddress, _splitsAddress, _hypercertFractionId, poolId, destinationOutputTokenAddress);
         gasService.payNativeGasForContractCallWithToken{ value: (inputTokenAddress != address(0)) ? msg.value : (msg.value - amount)}(
             address(this),
             destinationChain,
@@ -260,33 +364,47 @@ contract SwapperBridger is AxelarExecutable, ReentrancyGuard {
         string calldata tokenSymbol,
         uint256 amount
     ) internal override {
-        (address hypercertRecipient, address recipientAddress, uint256 hypercertFractionId, uint256 poolId) = abi.decode(payload, (address, address, uint256, uint256));
+        (address hypercertRecipient, address recipientAddress, uint256 hypercertFractionId, uint256 poolId, address outputTokenAddress) = abi.decode(payload, (address, address, uint256, uint256, address));
         uint256 amountOut;
         // Swap and transfer funds to allo pool if poolId is provided or else to recipient
         if (poolId != 0) {
-          amountOut = _swapAxlUsdcToUsdc(amount, address(this));
+          (amountOut, outputTokenAddress) = _swapTokenDestination(outputTokenAddress, amount, address(this));
 
           // Approve the Allo contract to withdraw funds
-          require(IERC20(USDC).approve(recipientAddress, amount), "Approval failed");
+          require(IERC20(outputTokenAddress).approve(recipientAddress, amount), "Approval failed");
           
           // Fund allo pool
-          IAllo(recipientAddress).fundPool(poolId, amountOut);
+          try IAllo(recipientAddress).fundPool(poolId, amountOut) {
+            emit AlloPoolFunded(poolId, amountOut, outputTokenAddress);
+          } catch (bytes memory reason) {
+            // Fallback to transferring output tokens to EOA
+            require(IERC20(outputTokenAddress).transfer(hypercertRecipient, amountOut), "Transfer failed");
+            emit AlloPoolFundingFailed(poolId, amountOut, outputTokenAddress, reason);
+          }
         } else {
-          amountOut = _swapAxlUsdcToUsdc(amount, recipientAddress);
+          (amountOut, outputTokenAddress) = _swapTokenDestination(outputTokenAddress, amount, recipientAddress);
         }
         
-        _receiveCrossChain(USDC, amountOut, recipientAddress, hypercertFractionId, hypercertRecipient);
-   
+        _receiveCrossChain(amount, recipientAddress, hypercertFractionId, hypercertRecipient);
+
+        emit CrossChainTransfer(
+            msg.sender,
+            outputTokenAddress,
+            amountOut,
+            recipientAddress,
+            hypercertFractionId,
+            hypercertRecipient
+        );
+        
         emit Executed();
     }
 
     function _receiveCrossChain(
-        address _token,
         uint256 _amount,
         address _splitsAddress,
         uint256 _hypercertFractionId,
         address _fractionRecipient
-    ) internal nonReentrant {
+    ) internal {
         require(_splitsAddress != address(0), "Invalid splits address");
         require(_fractionRecipient != address(0), "Invalid recipient address");
 
@@ -301,29 +419,18 @@ contract SwapperBridger is AxelarExecutable, ReentrancyGuard {
             uint256[] memory updatedFractionBalances = new uint256[](2);
             updatedFractionBalances[0] = availableBalance - _amount;
             updatedFractionBalances[1] = _amount;
-            
-            // Encode the function call to splitFraction
-            bytes memory data = abi.encodeWithSignature(
-                "splitFraction(address,uint256,uint256[])",
-                _fractionRecipient,
-                _hypercertFractionId,
-                updatedFractionBalances
-            );
 
             // Call the function with the contract as msg.sender
-            (bool success, ) = hypercertContract.call(data);
-            require(success, "Function call failed");
-        }
+            try IHypercertToken(hypercertContract).splitFraction(
+              _fractionRecipient,
+              _hypercertFractionId,
+              updatedFractionBalances
+            ) {
 
-        // Celebrate
-        emit CrossChainTransfer(
-            msg.sender,
-            _token,
-            _amount,
-            _splitsAddress,
-            _hypercertFractionId,
-            _fractionRecipient
-        );
+            } catch (bytes memory reason) {
+              emit HypercertTransferFailed(reason);
+            }
+        }
     }
 
     /**
@@ -374,5 +481,15 @@ contract SwapperBridger is AxelarExecutable, ReentrancyGuard {
         emit TokenSwapped(AXL_USDC, USDC, _amount, usdcAmount);
 
         return usdcAmount;
+    }
+
+    /// @notice Enable onwer of the contract to transfer tokens that have been mistakenly sent to it.
+    /// There is no custody risk as this contract is not meant to hold any funds in between users calls.
+    /// @dev Only owner can call.
+    /// @param token Address of the ERC20 token to be transfered.
+    /// @param to Address that will receive the tokens.
+    /// @param amount Amount of ERC20 or native tokens to transfer.
+    function rescueFunds(address token, address payable to, uint256 amount) external onlyOwner {
+        require(IERC20(token).transfer(to, amount), "Transfer failed");
     }
 }
